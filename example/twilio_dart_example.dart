63b0f30d8775e3e18661f3245f47388994686a05
thameemkj <thameem.kj@gmail.com>
2021-12-01 12:59:36 +0530

    First commit

--- example/twilio_dart_example.dart
+++ example/twilio_dart_example.dart
@@ -1,9 +1,8 @@
 import 'package:twilio_dart/twilio_dart.dart';
 
-const String ACCOUNT_SID = 'AC91c690e71689338c4a73cb594f6db84f';
-const String AUTH_TOKEN = 'a859bf11058892b902f4f98b71e1d723';
+const String ACCOUNT_SID = '';
+const String AUTH_TOKEN = '';
 const String FROM_NUMBER = '+19122748564';
-//const String TO_NUMBER = '+919744890302';
 const String TO_NUMBER = '+919400049950';
 
 void main() async {
--- todelete/x.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-void main() async {
-  var request = await HttpClient().getUrl(Uri.parse('https://www.google.com'));
-  var response = await request.close();
-  response.listen((event) {
-    print(String.fromCharCodes(event));
-  });
-}
